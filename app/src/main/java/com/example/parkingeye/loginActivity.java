a089ac2c9721cf607fa391d014e914f26f5e2a7d
genie <genie.in.earth@gmail.com>
2024-12-03 13:20:59 +0900

    [Feat] 자바코드 구현

--- app/src/main/java/com/example/parkingeye/loginActivity.java
+++ app/src/main/java/com/example/parkingeye/loginActivity.java
@@ -1,4 +1,12 @@
 package com.example.parkingeye;
 
-public class loginActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class loginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
 }
--- app/src/main/java/com/example/parkingeye/openingActivity.java
+++ app/src/main/java/com/example/parkingeye/openingActivity.java
@@ -1,4 +1,24 @@
 package com.example.parkingeye;
 
-public class openingActivity {
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class openingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.opening); // opening.xml 연결
+
+        // 5초 후 login 화면으로 전환
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(openingActivity.this, loginActivity.class);
+            startActivity(intent);
+            finish(); // 현재 Activity를 종료하여 뒤로 가기 버튼으로 돌아가지 않도록 함
+        }, 5000); // 5000ms = 5초
+    }
 }
